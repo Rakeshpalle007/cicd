4a018d5053f0ad8ce74da8588ab3177b9a19d893
Rakeshpalle007 <rakeshpalle007@gmail.com>
2025-02-06 17:42:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, Hope you are doing well !! this is gcp cloud platform');
+  res.send('Hello World, Hope you are doing well !! this is devops engineer');
 });
 
 app.listen(PORT, HOST);
