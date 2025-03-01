cc8e6cd8ddc92d3e932d70177d0cdf9f14f62577
Rakeshpalle007 <rakeshpalle007@gmail.com>
2025-03-01 18:18:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hi ra sweetygaa,and pichidi');
+  res.send('Hi ra sweetygaa & m dng pichidhana..................');
 });
 
 app.listen(PORT, HOST);
