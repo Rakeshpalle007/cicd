b256f5bf641c7c2424c554e597393fd930d81888
Rakeshpalle007 <rakeshpalle007@gmail.com>
2025-09-01 16:32:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello from App Engine!'
+@app.route("/")
+def home():
+    return "Hello from App Engine!"
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # ✅ Must use PORT
+    app.run(host="0.0.0.0", port=port)        # ✅ Must bind to 0.0.0.0
+
