55689786050fad0a8fbfafc641dd20757f397fab
akulsai <akulsai.4v@gmail.com>
2025-09-01 17:05:40 +0530

    Deploy Python app using GitHub Actions

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return '✅ App is working!'
-
-@app.route('/healthz')
-def healthz():
-    return 'OK', 200
+def index():
+    return "✅ Hello from App Engine + GitHub Actions!"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))  # required for App Engine
-    app.run(host='0.0.0.0', port=port)        # bind to external host
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
